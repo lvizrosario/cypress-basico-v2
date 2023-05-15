04a1b8670534f49b666bafe70aaf5f5c606b1f26
Luiz Carlos <lucafi.araujo@gmail.com>
2023-05-15 18:22:23 -0300

    removing test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -211,12 +211,4 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
       cy.contains('Talking About Testing').should('be.visible')
     })
-
-    it('acessa a página da política de privacidade removendo o target e então clicando no link', function() {
-      cy.get('a')
-        .invoke('removeAttr', 'target')
-        .click()
-
-      cy.contains('Talking About Testing').should('be.visible')
-    })
 })
